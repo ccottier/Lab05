5bb1c06df8874d1046ad94eae021f0974ed9552f
0smium <steve.walsh@transfueler.com>
2017-02-04 14:33:11 -0800

    uncommented all problems and commented out test statements

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 // Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 // "The sum of 4 and 7 is 11."
 // Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
-//
-// // Write your code here
-// function testSum(x, y) {
-//   var sum = [(x + y), 'The sum of ' + x + ' and ' + y + ' is ' + (x + y) + '.'];
-//   return sum;
-// }
-// // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
-//
+
+// Write your code here
+function sum(x, y) {
+  var sum = [(x + y), 'The sum of ' + x + ' and ' + y + ' is ' + (x + y) + '.'];
+  return sum;
+}
+// Here is the test for sum(); uncomment it to run it
+//testSum(4, 7);
+
 // // Once you get the test passing, do an a-c-p cycle, synchronize the code between both GitHubs and both laptops, and swap Driver/Navigator roles. Don't forget to create a new branch for your work on the next question!
 //
 // /////////////////////////////////////
@@ -19,18 +19,18 @@
 // Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 // "The product of 4 and 7 is 28."
 // Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
-//
-// // Write your code here
-//
-// // Here is the test for multiply(); uncomment it to run it
-//
-//
-// function testMultiply(x, y) {
-//   var multiply = [(x * y), 'The product of ' + x + ' and ' + y + ' is ' + (x * y) + '.'];
-//   return multiply;
-// }
-//
-// testMultiply(4, 7);
+
+// Write your code here
+
+// Here is the test for multiply(); uncomment it to run it
+
+
+function multiply(x, y) {
+  var multiply = [(x * y), 'The product of ' + x + ' and ' + y + ' is ' + (x * y) + '.'];
+  return multiply;
+}
+
+//testMultiply(4, 7);
 // Once you get the test passing, do an a-c-p cycle, synchronize the code between both GitHubs and both laptops, and swap Driver/Navigator roles. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
@@ -41,12 +41,12 @@ Fourth element: "The product of 4 and 7 and 5 is 140."
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-// function testSumAndMultiply(x, y, z) {
-//   var sumAndMultiply = [(x + y + z), (x * y * z), x + ' and ' + y + ' and ' + z + ' sum to ' + (x + y + z) + '.', 'The product of ' + x + ' and ' + y + ' and ' + z + ' is ' + (x * y * z) + '.'];
-//   return sumAndMultiply;
-// }
-// // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+function sumAndMultiply(x, y, z) {
+  var sumAndMultiply = [(x + y + z), (x * y * z), x + ' and ' + y + ' and ' + z + ' sum to ' + (x + y + z) + '.', 'The product of ' + x + ' and ' + y + ' and ' + z + ' is ' + (x * y * z) + '.'];
+  return sumAndMultiply;
+}
+// Here is the test for sumAndMultiply(); uncomment it to run it
+//testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle, synchronize the code between both GitHubs and both laptops, and swap Driver/Navigator roles. don't forget to create a new branch for your work on the next question!
 
@@ -55,17 +55,16 @@ Test this function by hand in the console to get it working, and when you think 
 Write a function called sumArray() that takes in an array of numbers as its argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
-//
-// // Write your code here
+
+// Write your code here
 testArray = [2,3,4];
-// function testSumArray(arr) {
-//   var answerArray = [arr[0] + arr[1] + arr[2], arr[0] + ',' + arr[1] + ',' + arr[2] + ' was passed in as an array of numbers, and ' + (arr[0] + arr[1] + arr[2]) + ' is their sum.'];
-//   return answerArray;
-// }
-//
-// // var answerArray = [9, "2,3,4 was passed in as an array of numbers, and 9 is their sum."]
-// // // Here is the test for sumArray(); uncomment it to run it
-// testSumArray(testArray);
+function sumArray(arr) {
+  var answerArray = [arr[0] + arr[1] + arr[2], arr[0] + ',' + arr[1] + ',' + arr[2] + ' was passed in as an array of numbers, and ' + (arr[0] + arr[1] + arr[2]) + ' is their sum.'];
+  return answerArray;
+}
+
+// Here is the test for sumArray(); uncomment it to run it
+//testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle, synchronize the code between both GitHubs and both laptops, and swap Driver/Navigator roles. Don't forget to create a new branch for your work on the next question!
 
@@ -76,12 +75,12 @@ Write a function called multiplyArray() that takes an array of numbers as its ar
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
-function testMultiplyArray(arr) {
+function multiplyArray(arr) {
   var answerArray = [arr[0] * arr[1] * arr[2], 'The numbers ' + arr[0] + ',' + arr[1] + ',' + arr[2] + ' have a product of ' + (arr[0] * arr[1] * arr[2]) + '.'];
   return answerArray;
 }
 // Here is the test for multiplyArray(); uncomment it to run it
-testMultiplyArray(testArray);
+//testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle, synchronize the code between both GitHubs and both laptops, and swap Driver/Navigator roles. Submit the link to the repo via Canvas.
 
