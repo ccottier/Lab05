ea48685051e2e4a909f41ba9614cdd259e72cf50
0smium <steve.walsh@transfueler.com>
2017-02-04 14:34:52 -0800

    uncommented all test code for review

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function sum(x, y) {
   return sum;
 }
 // Here is the test for sum(); uncomment it to run it
-//testSum(4, 7);
+testSum(4, 7);
 
 // // Once you get the test passing, do an a-c-p cycle, synchronize the code between both GitHubs and both laptops, and swap Driver/Navigator roles. Don't forget to create a new branch for your work on the next question!
 //
@@ -30,7 +30,7 @@ function multiply(x, y) {
   return multiply;
 }
 
-//testMultiply(4, 7);
+testMultiply(4, 7);
 // Once you get the test passing, do an a-c-p cycle, synchronize the code between both GitHubs and both laptops, and swap Driver/Navigator roles. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
@@ -46,7 +46,7 @@ function sumAndMultiply(x, y, z) {
   return sumAndMultiply;
 }
 // Here is the test for sumAndMultiply(); uncomment it to run it
-//testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle, synchronize the code between both GitHubs and both laptops, and swap Driver/Navigator roles. don't forget to create a new branch for your work on the next question!
 
@@ -64,7 +64,7 @@ function sumArray(arr) {
 }
 
 // Here is the test for sumArray(); uncomment it to run it
-//testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle, synchronize the code between both GitHubs and both laptops, and swap Driver/Navigator roles. Don't forget to create a new branch for your work on the next question!
 
@@ -80,7 +80,7 @@ function multiplyArray(arr) {
   return answerArray;
 }
 // Here is the test for multiplyArray(); uncomment it to run it
-//testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle, synchronize the code between both GitHubs and both laptops, and swap Driver/Navigator roles. Submit the link to the repo via Canvas.
 
@@ -95,42 +95,42 @@ function multiplyArray(arr) {
 /////////////////////////////////////
 /////////////////////////////////////
 
-// function testSum() {
-//   if (sum(4,7)[1] === 'The sum of 4 and 7 is 11.') {
-//     console.log('%c TEST FOR sum() PASSES', 'color: green');
-//   } else {
-//     console.log('%c TEST FOR sum() FAILS', 'color: red');
-//   }
-// }
-//
-// function testMultiply() {
-//   if (multiply(5,9)[1] === 'The product of 5 and 9 is 45.') {
-//     console.log('%c TEST FOR multiply() PASSES', 'color: green');
-//   } else {
-//     console.log('%c TEST FOR multiply() FAILS', 'color: red');
-//   }
-// }
-//
-// function testSumAndMultiply() {
-//   if (sumAndMultiply(4, 7, 5)[2] === '4 and 7 and 5 sum to 16.' && sumAndMultiply(4, 7, 5)[3] === 'The product of 4 and 7 and 5 is 140.') {
-//     console.log('%c TEST FOR sumAndMultiply() PASSES', 'color: green');
-//   } else {
-//     console.log('%c TEST FOR sumAndMultiply() FAILS', 'color: red');
-//   }
-// }
-//
-// function testSumArray() {
-//   if (sumArray(testArray)[1] === '2,3,4 was passed in as an array of numbers, and 9 is their sum.') {
-//     console.log('%c TEST FOR sumArray() PASSES', 'color: green');
-//   } else {
-//     console.log('%c TEST FOR sumArray() FAILS', 'color: red');
-//   }
-// }
-//
-// function testMultiplyArray() {
-//   if (multiplyArray(testArray)[1] === 'The numbers 2,3,4 have a product of 24.') {
-//     console.log('%c TEST FOR multiplyArray() PASSES', 'color: green');
-//   } else {
-//     console.log('%c TEST FOR multiplyArray() FAILS', 'color: red');
-//   }
-// }
+function testSum() {
+  if (sum(4,7)[1] === 'The sum of 4 and 7 is 11.') {
+    console.log('%c TEST FOR sum() PASSES', 'color: green');
+  } else {
+    console.log('%c TEST FOR sum() FAILS', 'color: red');
+  }
+}
+
+function testMultiply() {
+  if (multiply(5,9)[1] === 'The product of 5 and 9 is 45.') {
+    console.log('%c TEST FOR multiply() PASSES', 'color: green');
+  } else {
+    console.log('%c TEST FOR multiply() FAILS', 'color: red');
+  }
+}
+
+function testSumAndMultiply() {
+  if (sumAndMultiply(4, 7, 5)[2] === '4 and 7 and 5 sum to 16.' && sumAndMultiply(4, 7, 5)[3] === 'The product of 4 and 7 and 5 is 140.') {
+    console.log('%c TEST FOR sumAndMultiply() PASSES', 'color: green');
+  } else {
+    console.log('%c TEST FOR sumAndMultiply() FAILS', 'color: red');
+  }
+}
+
+function testSumArray() {
+  if (sumArray(testArray)[1] === '2,3,4 was passed in as an array of numbers, and 9 is their sum.') {
+    console.log('%c TEST FOR sumArray() PASSES', 'color: green');
+  } else {
+    console.log('%c TEST FOR sumArray() FAILS', 'color: red');
+  }
+}
+
+function testMultiplyArray() {
+  if (multiplyArray(testArray)[1] === 'The numbers 2,3,4 have a product of 24.') {
+    console.log('%c TEST FOR multiplyArray() PASSES', 'color: green');
+  } else {
+    console.log('%c TEST FOR multiplyArray() FAILS', 'color: red');
+  }
+}
